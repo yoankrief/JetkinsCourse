94bcf06733b2e8e175443e7df1aa6f9f96cd00a5
Yoan Krief <yoankr@gmail.com>
2022-03-13 14:14:02 +0200

    adding tests

--- simple_webserver/tests/test_flask_web.py
+++ simple_webserver/tests/test_flask_web.py
@@ -10,7 +10,7 @@ class TestFlaskWebServer(unittest.TestCase):
         self.fail("shouldn't happen")
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO2')
+        self.assertEqual('foo'.upper(), 'FOO')
 
     def test_hello(self):
         results = hello('Dan')
